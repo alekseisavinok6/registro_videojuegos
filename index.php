4385e05c756288a9c850cbb21e732d0f3d7c2508
alekseisavinok6 <alekseisavinok6@gmail.com>
2024-12-13 23:59:16 +0100

    v1.0

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Videojuegos playstation 2</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link href="recursos/css/bootstrap.min.css" rel="stylesheet">
+    <link href="recursos/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container py-3">
+        <h2 class="text-center">Videojuegos playstation 2</h2>
+        <div class="col-auto">
+            <a href="#" class="btn btn-warning"> Alta de datos</a>
+        </div>
+
+    </div>
+
+    <script src="recursos/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
